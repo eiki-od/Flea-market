4e879c711792434c81c7df11da7be5492967de7e
eiki-od <eiki12042060@gmail.com>
2020-07-22 16:31:42 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "flea-market"
-set :repo_url, "eiki-od/Flea-market"
+set :repo_url, 'git@github.com:eiki-od/Flea-market.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
