6784c0f046a1da9234e8972fad3aabe42a970319
eiki-od <eiki12042060@gmail.com>
2020-07-29 11:10:29 +0900

    haml change

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2020_07_28_031830) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
-    t.integer "age", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "kanji_family", null: false
@@ -39,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_07_28_031830) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.index ["age"], name: "index_users_on_age", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
