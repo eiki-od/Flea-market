3c25c535cc65f3981ce4a389dc993d95fc69f41a
eiki-od <eiki12042060@gmail.com>
2020-07-30 10:25:22 +0900

    Update 20200728031440_add_devise_to_users.rb

--- db/migrate/20200728031440_add_devise_to_users.rb
+++ db/migrate/20200728031440_add_devise_to_users.rb
@@ -5,7 +5,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :name,               null: false
-      t.integer :age,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :kanji_family,       null: false
