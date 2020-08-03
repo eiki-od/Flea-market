adb034c862bc4610d7117353524321529737249f
eiki-od <eiki12042060@gmail.com>
2020-08-03 20:04:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2020_07_30_064907) do
+ActiveRecord::Schema.define(version: 2020_07_29_065022) do
 
   create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
