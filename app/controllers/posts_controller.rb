6398b98455b91ab38c2621d2c2b1df30e6871b7d
nagamontech <67036025+nagamontech@users.noreply.github.com>
2020-08-03 17:49:53 +0900

    ビューの修正２

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,19 @@ class PostsController < ApplicationController
   end
   
   def show
+    # destoryに以下を記入すると、エラーになる。showに記入すると、詳細ページの更新でDBから削除が行われる。
+    # ８行目にbinding.pryを記述すると、値は取りに行っていることがわかる。▶︎記述内容は間違っていないと思う。
+    @post = Post.find(params[:id])
+    @post.destroy 
+<<<<<<< HEAD
+=======
+  end
+  
+  def destory
+>>>>>>> bc074e5afceedfc4fb9feecbefbe717597f4e73e
+  end
+  
+  def destory
   end
   
   def edit
