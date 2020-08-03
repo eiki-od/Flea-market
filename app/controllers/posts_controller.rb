6202605c70b35833bc611b0db6cf665b24202c84
ryo1090 <67303167+ryo1090@users.noreply.github.com>
2020-08-03 16:38:19 +0900

    commit(中途半端【マイページと削除機能】)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,13 @@ class PostsController < ApplicationController
   end
 
   def show
+    # destoryに以下を記入すると、エラーになる。showに記入すると、詳細ページの更新でDBから削除が行われる。
+    # ８行目にbinding.pryを記述すると、値は取りに行っていることがわかる。▶︎記述内容は間違っていないと思う。
+    @post = Post.find(params[:id])
+    @post.destroy 
   end
   
   def destory
-    post = Post.find(params[:id])
-    post.destroy 
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_031442) do
+ActiveRecord::Schema.define(version: 2020_07_30_064907) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2020_07_28_031442) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
