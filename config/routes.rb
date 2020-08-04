fc8554f4812e76210fadfef88798925f1fdd4c9b
eiki-od <eiki12042060@gmail.com>
2020-08-04 15:00:49 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
-    delete 'logout', to: 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
   root to: "posts#index"
   resources :posts, only: [:index,:new, :show, :destroy]
-  resources :users, only: [:show, :destroy, :new]
+  resources :users, only: [:show, :new]
   # 12行目にnewを加えていますが、これがログアウト画面を表示させるためのものです。
   # ※７つのアクションのうち、使わなそうなアクションを選んで、ビューまで作るように言われてたので、この実装をしています。
   # それ以外、newにしているこだわりはありません。
