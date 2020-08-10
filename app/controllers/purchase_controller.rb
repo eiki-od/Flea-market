94a5ed736959b8fe5a57dd2006a2678158584eac
eiki-od <eiki12042060@gmail.com>
2020-08-10 14:00:44 +0900

    comit

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -19,7 +19,7 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => 135000, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
