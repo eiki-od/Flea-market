042160cf227786efed31ff5cba32170670e3ff05
nagamontech <67036025+nagamontech@users.noreply.github.com>
2020-08-12 11:43:49 +0900

    ビューの調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ApplicationRecord
     validates :delivery
     validates :price
     validates :delivery
+    validates :images
   end
   validates :name, length: { maximum: 40}
   mount_uploader :image, ImageUploader
